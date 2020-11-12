03040e4805e7beb15a1bacd9bb9dc34d21db21d3
Oscar Qvarford <oscar.qvarford@gmail.com>
2020-11-12 11:20:53 +0100

    continued work on hero

--- public/js/hero.js
+++ public/js/hero.js
@@ -2,22 +2,35 @@ const canvas = document.getElementById('hero');
 const ctx = canvas.getContext('2d');
 
 let h, w;
+let drawIndex = 0;
+let animationLength = 1000;
 
 const draw = () => {
+  //if (h && w) ctx.clearRect(0, 0, h, w);
+
   h = window.innerHeight;
   w = window.innerWidth;
 
   canvas.width = w
   canvas.height = h / 2;
 
-  ctx.font = `${50}px Arial`;
+  let textSize = h;
+  ctx.font = `${textSize}px Arial`;
   const text = 'Katedralskolans pingissamfund';
-  const textMetrics = ctx.measureText(text);
-  const textWidth = textMetrics.width;
-  const textHeight = textMetrics.actualBoundingBoxAscent + textMetrics.actualBoundingBoxDescent;
-  widthHeightRatio = textWidth / textHeight;
-  ctx.strokeText(text, 5, 100);
+  const textWidth = ctx.measureText(text).width;
+  const widthSizeRatio = textWidth / textSize;
+  const targetWidth = (2 / 3) * w;
+  textSize = targetWidth / widthSizeRatio;
+  ctx.font = `${textSize}px Arial`;
+  ctx.strokeText(text, 20, 100);
+
+  drawIndex++;
+
+  if (drawIndex >= animationLength) clearInterval(animationInterval);
 }
+
+const animationInterval = setInterval(draw, 1000 / 60);
+
 draw();
 
 window.addEventListener('resize', draw);
