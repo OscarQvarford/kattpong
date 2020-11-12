c997a808b615ed0d8c4f4c3834bef99cf4ec1950
Oscar Qvarford <oscar.qvarford@gmail.com>
2020-11-12 14:14:27 +0100

    animated dot over 'i' on hero text

--- public/js/hero.js
+++ public/js/hero.js
@@ -3,11 +3,14 @@ const ctx = canvas.getContext('2d');
 
 let h, w;
 let drawIndex = 0;
-let animationLength = 0.3;
+let slideAnimationLength = 0.3;
+let bounceAnimationLength = 0.8;
 let endYOffset = 50;
 let secondsPassed = 0;
 
 const draw = () => {
+  if (h && w) ctx.clearRect(0, 0, w, h);
+
   h = window.innerHeight;
   w = window.innerWidth;
 
@@ -26,23 +29,51 @@ const draw = () => {
   iMetrics = ctx.measureText('i');
   iWidth = iMetrics.width;
   iActualBoundingBoxAscent = iMetrics.actualBoundingBoxAscent;
-  console.log(iMetrics)
   beforeiWidth = ctx.measureText(text.substr(0, text.indexOf('i'))).width;
   ctx.fillStyle = 'rgba(255,255,255,1)';
 
-  if (drawIndex >= 60 * animationLength) {
-    clearInterval(animationInterval);
+  const easeInOut = (t, b, c, d) => {
+    return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b;
+  }
 
+  const postAnimationDraw = () => {
     ctx.strokeText(text, 20, 130);
     ctx.fillRect(beforeiWidth + 20, 130 - iActualBoundingBoxAscent - 2, iWidth, iWidth);
-  } else {
-    const easeInOut = (t, b, c, d) => {
-      return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b;
-    }
+  }
 
-    ctx.strokeStyle = `rgba(0,0,0,${easeInOut(secondsPassed, 0, 1, 0.3)})`;
-    ctx.strokeText(text, 20, easeInOut(secondsPassed, 100, 30, 0.3));
-    ctx.fillRect(beforeiWidth + 20, easeInOut(secondsPassed, 100, 30, 0.3) - iActualBoundingBoxAscent - 2, iWidth, iWidth);
+  if (drawIndex >= 60 * slideAnimationLength && drawIndex >= 60 * bounceAnimationLength) {
+    clearInterval(animationInterval);
+    postAnimationDraw();
+
+    ctx.beginPath();
+    ctx.arc(beforeiWidth + 20 + iWidth / 2,  130 - iActualBoundingBoxAscent, iWidth / 2, 0, 2 * Math.PI);
+    ctx.stroke();
+  } else {
+    if (drawIndex < 60 * slideAnimationLength) {
+      ctx.strokeStyle = `rgba(0,0,0,${easeInOut(secondsPassed, 0, 1, 0.3)})`;
+      ctx.strokeText(text, 20, easeInOut(secondsPassed, 100, 30, 0.3));
+      ctx.fillRect(beforeiWidth + 20, easeInOut(secondsPassed, 100, 30, 0.3) - iActualBoundingBoxAscent - 2, iWidth, iWidth);
+    } else postAnimationDraw();
+
+    if (drawIndex < 60 * 0.4) {
+      ctx.beginPath();
+      ctx.arc(beforeiWidth + 20 + iWidth / 2, 
+              easeInOut(secondsPassed, 100, 30, 0.4) - iActualBoundingBoxAscent, 
+              iWidth / 2, 0, 2 * Math.PI);
+      ctx.stroke();
+    } else if (drawIndex < 60 * 0.6) {
+      ctx.beginPath();
+      ctx.arc(beforeiWidth + 20 + iWidth / 2, 
+              easeInOut(secondsPassed, 130, -10, 0.2) - iActualBoundingBoxAscent, 
+              iWidth / 2, 0, 2 * Math.PI);
+      ctx.stroke();
+    } else if (drawIndex < 60 * 0.8) {
+      ctx.beginPath();
+      ctx.arc(beforeiWidth + 20 + iWidth / 2, 
+              easeInOut(secondsPassed, 130, -10, 0.2) - iActualBoundingBoxAscent, 
+              iWidth / 2, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
   }
 
   secondsPassed += 1000 / 60 / 1000;
