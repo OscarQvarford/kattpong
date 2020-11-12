38c535ffd4f60eb7a74939baf197d9fa644fbcb3
Oscar Qvarford <oscar.qvarford@gmail.com>
2020-11-12 13:13:59 +0100

    covered dot on first 'i' on hero text

--- public/js/hero.js
+++ public/js/hero.js
@@ -23,10 +23,18 @@ const draw = () => {
   textSize = targetWidth / widthSizeRatio;
   ctx.font = `${textSize}px Arial`;
 
+  iMetrics = ctx.measureText('i');
+  iWidth = iMetrics.width;
+  iActualBoundingBoxAscent = iMetrics.actualBoundingBoxAscent;
+  console.log(iMetrics)
+  beforeiWidth = ctx.measureText(text.substr(0, text.indexOf('i'))).width;
+  ctx.fillStyle = 'rgba(255,255,255,1)';
+
   if (drawIndex >= 60 * animationLength) {
     clearInterval(animationInterval);
 
     ctx.strokeText(text, 20, 130);
+    ctx.fillRect(beforeiWidth + 20, 130 - iActualBoundingBoxAscent - 2, iWidth, iWidth);
   } else {
     const easeInOut = (t, b, c, d) => {
       return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b;
@@ -34,6 +42,7 @@ const draw = () => {
 
     ctx.strokeStyle = `rgba(0,0,0,${easeInOut(secondsPassed, 0, 1, 0.3)})`;
     ctx.strokeText(text, 20, easeInOut(secondsPassed, 100, 30, 0.3));
+    ctx.fillRect(beforeiWidth + 20, easeInOut(secondsPassed, 100, 30, 0.3) - iActualBoundingBoxAscent - 2, iWidth, iWidth);
   }
 
   secondsPassed += 1000 / 60 / 1000;
